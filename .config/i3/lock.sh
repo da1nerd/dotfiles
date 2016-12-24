09ff8fb3ac4a48a97460bb4cdb7347afb1cd9aab
Joel Lonbeck <joel@neutrinographics.com>
2016-12-23 18:00:51 -0800

    updated lock screen

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+ICON=~/.config/i3/lock.png
+TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2
-# org.mpris.MediaPlayer2.Player.Stop
-i3lock -u -i /tmp/screen.png
-rm /tmp/screen.png
+convert $TMPBG -scale 10% -scale 1000% $TMPBG
+convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+i3lock -u -i $TMPBG
