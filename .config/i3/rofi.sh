0644c92799d8bbd72e582723e589b37e28791679
Joel Lonbeck <joel@neutrinographics.com>
2016-12-23 17:24:33 -0800

    fixed rofi config

---
+++ .config/i3/rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rofi -color-window "argb:aa2f343f, argb:02f343f, argb:02f343f" -color-normal "argb:02f343f, #f3f4f5, argb:02f343f, argb:02f343f, #f3f4f5" -color-active "argb:02f343f, #887cda, argb:02f343f, argb:02f343f, #887cda" -color-urgent "argb:02f343f, #e53935, #2f343f, argb:02f343f, #e53935" -show run -width 100 -padding 400 -font "System San Francisco Display 18" -eh 2 -lines 5
