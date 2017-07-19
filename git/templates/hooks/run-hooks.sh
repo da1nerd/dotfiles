319f83b24edf7afb23ec166232bfa3d9303ce023
Joel Lonbeck <joel@neutrinographics.com>
2017-07-18 23:23:42 -0700

    fixes to git hooks

--- git/templates/hooks/run-hooks.sh
+++ git/templates/hooks/run-hooks.sh
@@ -9,10 +9,12 @@ hooks=~/.dotfiles/git/hooks
 echo "Executing $hook_type hook(s)"
 
 for hook in $hooks/*.$hook_type; do
-	echo ""
-	echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
-	${hook}
-	EXIT_CODE=$((${EXIT_CODE} + $?))
+	if [ -e "$hook" ]; then
+		echo ""
+		echo "${COLOR_LIGHTPURPLE}Executing ${hook}${COLOR_NONE}"
+		${hook}
+		EXIT_CODE=$((${EXIT_CODE} + $?))
+	fi
 done
 
 if [ ${EXIT_CODE} -ne 0 ]; then
