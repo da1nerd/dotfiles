4a824c39cc806a3d7191b582cbdb65d427fb800f
Joel <joel@neutrinographics.com>
2020-02-11 15:48:00 +0700

    added atom and code installers

--- install/software.sh
+++ install/software.sh
@@ -6,3 +6,13 @@ sudo apt install ./keybase_amd64.deb
 rm keybase_amd64.deb
 run_keybase
 
+echo "Installing Atom"
+curl https://atom.io/download/deb -o atom_amd64.deb
+sudo apt install ./atom_amd64.deb
+rm atom-amd64.deb
+
+echo "Installing Visual Studio Code"
+# This link will need to be updated from time to time
+curl https://go.microsoft.com/fwlink/?LinkID=760868 -o code_amd64.deb
+sudo apt install ./code_amd64.deb
+rm code_amd64.deb
